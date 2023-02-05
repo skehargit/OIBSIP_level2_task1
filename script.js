eda9af4507dd65b70f03b9d6a098ee7fbe8b1d76
Sekhar1p <hackersekhar359@gmail.com>
2023-02-05 21:16:52 +0530

    calculator task done

---
+++ script.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    let last = display.innerText.split('')
+    
+  if(last[last.length-1]=="-"&&last[last.length-2]=="-"||last[last.length-1]=="*"&&last[last.length-2]=="*"||last[last.length-1]=="/"&&last[last.length-2]=="/"||last[last.length-1]+last[last.length-2]=="++"){
+    // last[last.length-1]="+"
+    display.innerText = display.innerText.slice(0,-1);
+    // console.log(last)
+  }
+    if (item.value == "AC") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      display.innerText = display.innerText.slice(0,-1);
+    } else if (display.innerText != "" && item.value == "=") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.value == "="||display.innerText == "" && item.value == "+" ||display.innerText == "" && item.value == "*" ||display.innerText == "" && item.value == "/" ) {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 500);
+    } else {
+      display.innerText += item.value;
+    }
+  };
+});
